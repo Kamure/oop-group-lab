8df28c7c11884d059668db1f5bfd366dd9319f65
Daniwanjohi <daniella.wanjohi@strathmore.edu>
2025-10-21 20:19:09 +0300

    Updated dashboard page

--- public/dashboard.php
+++ public/dashboard.php
@@ -11,14 +11,25 @@ $user = $_SESSION['user'];
 <head>
   <meta charset="UTF-8">
   <title>Dashboard</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
   <div class="dashboard">
     <h2>Welcome, <?php echo htmlspecialchars($user['name']); ?>!</h2>
     <p>Email: <?php echo htmlspecialchars($user['email']); ?></p>
     <p>Phone: <?php echo htmlspecialchars($user['phone']); ?></p>
-    <a href="logout.php">Logout</a>
+    <p>Your account is verified.</p><br></br>
+    <button class="btn-logout" onclick="confirmLogout()">Logout</button>
+
+    <script>
+    function confirmLogout() {
+     if (confirm("Are you sure you want to log out?")) {
+      window.location.href = 'logout.php';
+     }
+    }
+</script>
+
+
   </div>
 </body>
 </html>
