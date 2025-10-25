f798308b18e7cf98cd042167dace49533a7e08a6
kamure beyonce kabura <beyonce.kamure@strathmore.edu>
2025-10-25 09:46:44 +0300

    added a service class

---
+++ classes/Service.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/../config/Database.php';
+
+class Service {
+  private $db;
+
+  public function __construct() {
+    $this->db = (new Database())->pdo;
+  }
+
+  public function getAllServices() {
+    return $this->db->query("SELECT * FROM services")->fetchAll(PDO::FETCH_ASSOC);
+  }
+}
+?>
